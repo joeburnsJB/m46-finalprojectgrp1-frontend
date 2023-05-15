5f23b454dfc840888b20ee835718d80c6cd8e900
Alexc73 <127415658+Alexc73@users.noreply.github.com>
2023-05-15 14:20:59 +0100

    added search function

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,6 +5,9 @@ import { useEffect, useState } from 'react';
 
 export default function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
   useEffect(() => {
     const handleClickOutside = (event) => {
@@ -26,6 +29,30 @@ export default function Header() {
     setIsMenuOpen((prevIsMenuOpen) => !prevIsMenuOpen);
   };
 
+  const handleInputChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const handleSearch = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await fetch(
+        `https://www.cheapshark.com/api/1.0/games?title=${searchQuery}&limit=10&exact=0`
+      );
+      const data = await response.json();
+      setSearchResults(data);
+      setIsDropdownOpen(true);
+    } catch (error) {
+      console.log('Error fetching games:', error);
+    }
+  };
+
+  const handleResultClick = (gameID) => {
+    // Handle the click event for a specific result item
+    console.log(`Clicked gameID: ${gameID}`);
+    setIsDropdownOpen(false); // Close the dropdown after clicking
+  };
+
   return (
     <header className='header-container'>
       <section className='logo-section'>
@@ -33,9 +60,25 @@ export default function Header() {
         <img src={logo} alt="logo" className='logo' />
       </section>
       <div className='search-bar'>
-        <form action="">
-          <input type="text" placeholder="Search here.." name="search"/>
+        <form onSubmit={handleSearch}>
+          <input
+            type="text"
+            placeholder="Search here.."
+            name="search"
+            value={searchQuery}
+            onChange={handleInputChange}
+          />
+          <button className='search-button' type="submit">Search</button>
         </form>
+        {isDropdownOpen && (
+          <div className='search-dropdown'>
+            {searchResults.map((game) => (
+              <div key={game.gameID} onClick={() => handleResultClick(game.gameID)}>
+                {game.external}
+              </div>
+            ))}
+          </div>
+        )}
       </div>
       <div className={`hamburger-menu ${isMenuOpen ? 'active' : ''}`} onClick={toggleMenu}>
         <div className='bar'></div>
