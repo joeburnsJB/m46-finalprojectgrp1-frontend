a196c414a9fc9b2f7e6e0706315c3a7612e353fb
Alexc73 <127415658+Alexc73@users.noreply.github.com>
2023-05-15 10:22:04 +0100

    Links in hamburger menu

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import './App.css';
 
 import Header from './components/header/Header';
 import LoginRegister from './Pages/loginRegister/LoginRegister';
-import UsersList from "./Pages/userslist/UsersList";
+import UsersList from './Pages/userslist/UsersList';
 import Homepage from './Pages/homepage/Homepage';
+
+
 
 const App = () => {
   return (
@@ -12,11 +14,11 @@ const App = () => {
       <BrowserRouter>
       <Header />
         <Routes>
-          <Route path="/" element={<LoginRegister />} />
+          <Route path="/" element={<Homepage />} />
+          <Route path="/login-register" element={<LoginRegister />} />
           <Route path="/userlist" element={<UsersList />} />
-          <Route path="/home" element={<Homepage />} />
-        </Routes>
-      </BrowserRouter>
+          </Routes>
+          </BrowserRouter>
     </>
   );
 }
