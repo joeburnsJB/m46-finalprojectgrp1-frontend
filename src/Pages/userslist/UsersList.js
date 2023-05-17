0ff9d83d048201dcf5dfeea48d76964004daffd2
deergod <CommonTroll@live.co.uk>
2023-05-17 13:44:28 +0100

    styling pass for userslist js

--- src/Pages/userslist/UsersList.js
+++ src/Pages/userslist/UsersList.js
@@ -1,29 +1,29 @@
-import { useState, useEffect } from 'react';
-import './UsersList.css';
-import {getUsers} from "../../utils";
-import { Link } from 'react-router-dom';
+import { useState, useEffect } from "react"
+import "./UsersList.css"
+import {getUsers} from "../../utils"
+import { Link } from "react-router-dom"
 
 const UsersList = () => {
-  const [userList, setUserList] = useState([" "]);
-  const [loadingCheck, setloadingCheck] = useState(false);
+  const [userList, setUserList] = useState([" "])
+  const [loadingCheck, setloadingCheck] = useState(false)
   useEffect(() => {
     const fetchUserList = async () => {
         let data = await getUsers()
         setUserList(data)
         setloadingCheck(!loadingCheck)
-    };
-    fetchUserList();
+    }
+    fetchUserList()
   // eslint-disable-next-line
-  }, []);
+  }, [])
 
   return (
-    <div className='userlist-container'>
+    <div className="userlist-container">
       <h1>Users List</h1>
-      <ul className='user-list'>
+      <ul className="user-list">
         {userList ?
         <>
         {userList.map((username) => (
-          <li className='user-item'>{username}</li>
+          <li className="user-item">{username}</li>
         ))}
         </>
         :
@@ -34,15 +34,15 @@ const UsersList = () => {
             <button className="login-button">Login</button>
             </Link>
           :
-          <div className='loading-animation'>
-          <div className='loading-spinner'></div>
+          <div className="loading-animation">
+          <div className="loading-spinner"></div>
           </div>
           }
         </>
         }
       </ul>
     </div>
-  );
-};
+  )
+}
 
-export default UsersList;
+export default UsersList
