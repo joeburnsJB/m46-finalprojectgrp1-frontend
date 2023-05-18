b73c4f434200fd87a2a53ad21f8d8e946a41d816
Samra Junejo <samrajunejo1@gmail.com>
2023-05-18 15:11:13 +0100

    diffStoreArray global sr ux

--- src/Pages/searchpage/searchresults.js
+++ src/Pages/searchpage/searchresults.js
@@ -147,7 +147,7 @@ const SearchPage = () => {
           <br></br>
           <br></br>
           <h1>Stores (cheapest to most expensive)</h1>
-          <div id="diffStoreArray">
+          <div className="diffStoreArray">
             {dealLookUp.map((store, index) => {
               let storeIDForURL = store.storeID - 1
               let storeURL = "https://www.cheapshark.com/img/stores/banners/".concat(storeIDForURL).concat(".png")
